3f05a01bd3ecb7b73982ae1692e38f743bd1f284
AJ-Teposte <ada_hack184@hackademy.mx>
2022-03-13 00:32:18 -0800

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -1,9 +1,9 @@
 #Creando una función para sumar 2 numerotes
 
-def sum(x,y):
-  sum=int(x)+int(y)
-  print(sum)
+def suma(x,y):
+  suma=int(x)+int(y)
+  print(suma)
 
-sum(2,3)
+suma(2,3)
 
-sum(2.1,3)
+suma(2.1,3)
